3a92585192b8b8b7f51faff8f301972ef48733cd
AntePost <is1213791@gmail.com>
2019-04-05 13:44:49 +0300

    Add homework for lesson 6

--- internet_shop_vue/public/script.js
+++ internet_shop_vue/public/script.js
@@ -1,23 +1,50 @@
+Vue.component('search', {
+  template: `<input type="text" placeholder="Search" @input="$emit('input', $event.target.value)">`,
+});
+
+Vue.component('cart', {
+  props: ['cart', 'cart_total'],
+  template: `
+              <div class="cartWrapper">
+                <h3>Cart</h3>
+                <div class="cart" v-if="cart.length">
+                  <p v-for="cartItem in cart">There is {{ cartItem.quantity }} item of {{ cartItem.name }} in the cart with the price of {{ cartItem.price * cartItem.quantity }}</p>
+                  <p>The sum total of items in the cart - {{ cart_total }}</p>
+                </div>
+                <span v-if="!cart.length">The cart is empty</span>
+              </div>
+            `
+});
+
+Vue.component('error', {
+  props: ['response_code'],
+  template: `<p>There was an error with code {{ response_code }}`,
+});
+
 const app = new Vue({
   el: '#app',
   data: {
     items: [],
-    seachQuery: '',
+    searchQuery: '',
     cart: [],
+    responseCode: null,
   },
   mounted() {
     fetch('http://localhost:3000/products')
-      .then(response => response.json())
+      .then(response => {
+        this.responseCode = response.status;
+        return response.json();
+      })
       .then(items => {
-        items.forEach((item, i) => {
+        this.items = items.map((item, i) => {
           item.picSrc = `img/product${i + 1}.jpg`;
+          return item;
         });
-        this.items = items;
       });
   },
   computed: {
     filteredItems() {
-      const regEx = new RegExp(this.seachQuery, 'i');
+      const regEx = new RegExp(this.searchQuery, 'i');
       return this.items.filter(item => regEx.test(item.name));
     },
     cartTotal() {
